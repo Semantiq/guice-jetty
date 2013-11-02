2b9f314102e325ef036506da01a87ff277347848
Michal Jurczynski <michal.jurczynski@gmail.com>
2013-11-02 21:49:14 +0000

    Shuffling files around and making some orger in this template

---
+++ src/main/scala/eu/semantiq/webapp/SampleModule.scala
@@ -0,0 +1,17 @@
+package eu.semantiq.webapp
+
+import javax.servlet.http._
+
+import com.google.inject._
+
+class AModule extends AbstractModule {
+  def configure {
+    bind(classOf[MyServlet])
+  }
+}
+
+@Singleton
+class MyServlet extends HttpServlet {
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+  }
+}
--- src/main/scala/eu/semantiq/webapp/WebApp.scala
+++ src/main/scala/eu/semantiq/webapp/WebApp.scala
@@ -1,18 +1,19 @@
-package experiment
+package eu.semantiq.webapp
 
 import java.util.EnumSet._
 import javax.servlet.DispatcherType
-
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet._
-
 import com.google.inject._
 import com.google.inject.servlet._
+import com.google.inject.servlet.GuiceFilter
+import javax.servlet.DispatcherType
+import org.eclipse.jetty.servlet.DefaultServlet
 
 object WebApp extends App {
   Guice.createInjector(new ServletModule() {
 	override def configureServlets {
-		serve("/api/*").`with`(classOf[MyAppServlet])
+		serve("/api/*").`with`(classOf[MyServlet])
     }
   }, new AModule())
 
