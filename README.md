626d5db3e09236b8fbd64b61002efb7f61835cf0
Mateusz Jurczyński <mati@matixbook.(none)>
2013-11-03 14:33:18 +0100

    Updated README and finished front-end.

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ guice-jetty
 
 A sample self contained application with Guice and Jetty
 
-Start with `sbt run` and browse this URL for demo:
+Start with `sbt run` and browse to this URL for demo:
 [http://localhost:8080/static/index.html](http://localhost:8080/static/index.html)
+Press enter to go back to SBT.
+
+Technologies included:
+- [Google Guice](https://code.google.com/p/google-guice) - Dependency injection
+- [Jetty](http://eclipse.org/jetty) - HTTP server
+- [Salat](https://github.com/novus/salat) - JSON <-> case class object mapper, that is designed to use with [MongoDB](http://www.mongodb.org/)
--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -1,9 +1,16 @@
 function DemoCtrl($scope, $http) {
-  $scope.items = [ "default item 1", "default item 2" ];
-  $scope.caption = "Hello world!";
-  $scope.refresh = function() {
-	  $http.get("/api/demo.json").success(function(data) {
-		  $scope.items = data;
+  $scope.items = [ "Getting tasks..." ];
+  $scope.caption = "My tasks";
+  $scope.addNewTaskMessage = "Add new task:"
+  
+  $scope.addTask = function() {
+	  $http.post("/api/tasks", { task: $scope.newTask }).success(function(data) {
+		  $scope.items.push($scope.newTask);
+		  $scope.newTask = "";
 	  });
   };
+  
+  $http.get("/api/tasks").success(function(data) {
+	  $scope.items = data;
+  });
 }
